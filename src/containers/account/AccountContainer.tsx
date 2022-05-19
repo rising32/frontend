31043eec38fa830f6537185053c600de3b686659
HuaZhe Jin <jscrazy@yandex.com>
2022-05-19 09:15:05 +0800

    fix

--- src/containers/account/AccountContainer.tsx
+++ src/containers/account/AccountContainer.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { BriefCaseFillSvg, GearFillSvg, HouseCircleFillSvg, Person3FillSvg, PersonFillSvg } from '../../assets/svg';
+import CompanyInfo from '../../components/account/CompanyInfo';
 import PageHeader from '../../components/base/PageHeader';
-import ItemWithArrow from '../../components/common/ItemWithArrow';
-import RoundedView from '../../components/common/RoundedView';
 
 function AccountContainer() {
   return (
@@ -12,29 +10,7 @@ function AccountContainer() {
         <div className='font-bold'>Your ACCOUNT</div>
         <div>Sign out</div>
       </PageHeader>
-      <RoundedView className='bg-white text-black py-4 grid gap-2'>
-        <div className='text-center font-bold'>COMPANY PROFILE</div>
-        <ItemWithArrow Icon={PersonFillSvg}>
-          <span className='font-bold mr-2'>JEF</span>
-          <span>jfloubeyre@id-logistics.com</span>
-        </ItemWithArrow>
-        <ItemWithArrow Icon={HouseCircleFillSvg}>
-          <span className='font-bold mr-2'>JEF</span>
-          <span>jfloubeyre@id-logistics.com</span>
-        </ItemWithArrow>
-        <ItemWithArrow Icon={BriefCaseFillSvg}>
-          <span className='font-bold mr-2'>JEF</span>
-          <span>jfloubeyre@id-logistics.com</span>
-        </ItemWithArrow>
-        <ItemWithArrow Icon={GearFillSvg}>
-          <span className='font-bold mr-2'>JEF</span>
-          <span>jfloubeyre@id-logistics.com</span>
-        </ItemWithArrow>
-        <ItemWithArrow Icon={Person3FillSvg}>
-          <span className='font-bold mr-2'>JEF</span>
-          <span>jfloubeyre@id-logistics.com</span>
-        </ItemWithArrow>
-      </RoundedView>
+      <CompanyInfo />
     </div>
   );
 }
